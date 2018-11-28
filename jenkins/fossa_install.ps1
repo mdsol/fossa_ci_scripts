cc65e8ebb8e5ee20bbc20e3db968456647a899bd
Justin Giannone <jgiannone@mdsol.com>
2018-11-28 16:25:38 -0500

    Add console output statements to Jenkins PowerShell Scripts

--- jenkins/fossa_install.ps1
+++ jenkins/fossa_install.ps1
@@ -3,4 +3,6 @@
 [CmdletBinding()]
 Param()
 
+Write-Output "Installing FOSSA CLI..."
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/fossas/fossa-cli/master/install.ps1'))
+Write-Output "FOSSA CLI Installed Successfully!"
--- jenkins/fossa_run.ps1
+++ jenkins/fossa_run.ps1
@@ -3,10 +3,16 @@
 [CmdletBinding()]
 Param()
 
+Write-Output "Queuing FOSSA Checks..."
 . $env:ALLUSERSPROFILE\fossa-cli\fossa.exe
+Write-Output "FOSSA Checks Queued Successfully!"
 
 $FOSSA_FAIL_BUILD_TOGGLE = $env:FOSSA_FAIL_BUILD.toLower()
 if ($env:FOSSA_FAIL_BUILD_TOGGLE == "true") {
+    Write-Output "FOSSA Fail Build Flag Enabled.  Checking for Policy Violations..."
     . $env:ALLUSERSPROFILE\fossa-cli\fossa.exe test
-    exit $LastExitCode
+    $FOSSA_EXIT_CODE = $LASTEXITCODE
+    
+    Write-Output "FOSSA Policy Violations check returned : $FOSSA_EXIT_CODE (non-zero codes are failures)"
+    exit $FOSSA_EXIT_CODE
 }
