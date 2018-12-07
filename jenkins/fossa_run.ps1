2a204088e61f03d56d7ff592839b64db15804690
Justin Giannone <jgiannone@mdsol.com>
2018-12-07 12:26:27 -0500

    Fix Powershell Equality

--- jenkins/fossa_run.ps1
+++ jenkins/fossa_run.ps1
@@ -7,8 +7,12 @@ Write-Output "Queuing FOSSA Checks..."
 . $env:ALLUSERSPROFILE\fossa-cli\fossa.exe
 Write-Output "FOSSA Checks Queued Successfully!"
 
-$FOSSA_FAIL_BUILD_TOGGLE = $env:FOSSA_FAIL_BUILD.toLower()
-if ($env:FOSSA_FAIL_BUILD_TOGGLE == "true") {
+$FOSSA_FAIL_BUILD_TOGGLE = "false"
+if (Test-Path env:FOSSA_FAIL_BUILD) {
+    $FOSSA_FAIL_BUILD_TOGGLE = $env:FOSSA_FAIL_BUILD.toLower()
+}
+
+if ($env:FOSSA_FAIL_BUILD_TOGGLE -eq "true") {
     Write-Output "FOSSA Fail Build Flag Enabled.  Checking for Policy Violations..."
     . $env:ALLUSERSPROFILE\fossa-cli\fossa.exe test
     $FOSSA_EXIT_CODE = $LASTEXITCODE
