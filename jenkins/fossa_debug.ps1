844b60860a1af1bd0add11e884a1662911a08ef6
Justin Giannone <jgiannone@mdsol.com>
2019-08-09 11:18:38 -0400

    Add Jenkins Debug scripts

---
+++ jenkins/fossa_debug.ps1
@@ -0,0 +1,22 @@
+#Requires -Version 5
+
+[CmdletBinding()]
+Param()
+
+Write-Output "Queuing FOSSA Checks..."
+. $env:ALLUSERSPROFILE\fossa-cli\fossa.exe --debug
+Write-Output "FOSSA Checks Queued Successfully!"
+
+$FOSSA_FAIL_BUILD_TOGGLE = "false"
+if (Test-Path env:FOSSA_FAIL_BUILD) {
+    $FOSSA_FAIL_BUILD_TOGGLE = $env:FOSSA_FAIL_BUILD.toLower()
+}
+
+if ($env:FOSSA_FAIL_BUILD_TOGGLE -eq "true") {
+    Write-Output "FOSSA Fail Build Flag Enabled.  Checking for Policy Violations..."
+    . $env:ALLUSERSPROFILE\fossa-cli\fossa.exe test --debug
+    $FOSSA_EXIT_CODE = $LASTEXITCODE
+    
+    Write-Output "FOSSA Policy Violations check returned : $FOSSA_EXIT_CODE (non-zero codes are failures)"
+    exit $FOSSA_EXIT_CODE
+}
